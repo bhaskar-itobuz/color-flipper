5fed3b5e9f96294769bf3404c3032525108a93c1
Bhaskar Pal <bhaskar@itobuz.com>
2024-12-26 18:23:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function aqua() {
+    document.body.style.backgroundColor = "aqua";
+}
+
+function gray() {
+    document.body.style.backgroundColor = "gray";
+}
+
+function blue() {
+    document.body.style.backgroundColor = "blue";
+}
+
+function green() {
+    document.body.style.backgroundColor = "green";
+}
